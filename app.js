4641c9adf830c68ea91ba1af6fb2a9a4527b4bc3
Akshay <akshayhegde158@gmail.com>
2021-09-27 19:14:53 +0530

    assing to github

---
+++ app.js
@@ -0,0 +1,33 @@
+let input_text = document.getElementById("text_input");
+let output_text = document.getElementById("output");
+const btn = document.getElementById("btn-1");
+let url="https://api.funtranslations.com/translate/minion.json";
+
+
+function getURL(input) {
+  return url + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
+
+ btn.addEventListener('click', ()=>{
+  console.log("clicked")
+  let text = input_text.value;
+  console.log(text)
+  fetch(getURL(text))
+    .then(response => response.json())
+    .then(json => {
+      let translated_Text = json.contents.translated;
+      output_text.innerText = translated_Text;
+    })
+    .catch(errorHandler);
+ })
+  
+
+
+  
+
